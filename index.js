39cb971dce2bfc63be58f0e758f3a264255308e8
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-19 03:03:00 +0600

    all ok

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z2jpk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 async function run() {
     try {
